5c4ef518f944d0fad52e1d6a9e790a24c044b731
Rosa Linares <rosaelenalinaresacosta@egmail.com>
2021-05-28 17:33:26 +0200

    First commit

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-const publication = () => {
-  fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=pVkPx1LtoePFAHcgAkxwdwTmxCNqLB7w')
+let input = document.getElementById('mysearch')
+const search = document.querySelector('.btn')
+search.addEventListener('click', publication);
+
+
+
+function publication() {
+  fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${input.value}.json?api-key=pVkPx1LtoePFAHcgAkxwdwTmxCNqLB7w`)
     .then(response => response.json())
     .then((data) => {
-      console.log(data.results);
+
 
       const container = document.querySelector('.card')
-
+      container.innerHTML = ""
       data.results.books.forEach((books) => {
+
         const card = `<div class="post">
-                    <div class="rank">${books.rank}</div><h1>${books.title}</h1>
-                    <img src="${books.book_image}"></img>
-                    <h3>${books.author}</h3>
-                    <p>${books.description}</p>
-                    <button class="buy"><a href="${books.amazon_product_url}">Buy</a></button>
-                    </div>`
+                      <div class="first">
+                        <div class="rank">
+                          <div class="number" title="Ranking">${books.rank}</div>
+                        </div>
+                        <img src="${books.book_image}"></img>
+                      </div>
+                      <div class="second"
+                        <h1>${books.title}</h1>
+                        <h3>${books.author}</h3>
+                        <p>${books.description}</p>
+                        <button class="buy"><a href="${books.amazon_product_url}" target="_blank" title="Buy">Buy</a></button>
+                      </div>`
         container.insertAdjacentHTML('beforeend', card)
       })
 
-      const search = document.querySelector('.btn')
-      search.addEventListener('click', event => {
-        let input = document.getElementById('mysearch')
-        console.log(input.value)
-        publication(input.value)
-      })
+
     })
 };
-publication()
+
+
+
